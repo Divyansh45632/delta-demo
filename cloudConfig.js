c71def2de76e4e5a1fb4fea784567719511cc618
Divyansh45632 <166374835+Divyansh45632@users.noreply.github.com>
2025-10-07 19:59:04 +0530

    Create cloudConfig.js

---
+++ cloudConfig.js
@@ -0,0 +1,23 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "wanderlust_DEV",               // Folder name in Cloudinary
+    allowedFormats: ["jpg", "png", "jpeg"], // Allowed file types
+
+  },
+});
+
+module.exports = {
+  cloudinary,
+  storage,
+};
+
